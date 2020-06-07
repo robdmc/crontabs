073818919c538c8f05e399d1d9e1afdbba9b3187
Rob deCarvalho <robdmc@gmail.com>
2020-06-07 17:37:43 -0400

    updated publish script

--- publish.py
+++ publish.py
@@ -1,7 +1,9 @@
 import subprocess
 
 subprocess.call('pip install wheel'.split())
+subprocess.call('pip install twine'.split())
+subprocess.call('rm -rf ./build'.split())
+subprocess.call('rm -rf ./dist/'.split())
 subprocess.call('python setup.py clean --all'.split())
-subprocess.call('python setup.py sdist'.split())
-# subprocess.call('pip wheel --no-index --no-deps --wheel-dir dist dist/*.tar.gz'.split())
-subprocess.call('python setup.py register sdist bdist_wheel upload'.split())
+subprocess.call('python setup.py sdist bdist_wheel'.split())
+subprocess.call('twine upload dist/*'.split())
